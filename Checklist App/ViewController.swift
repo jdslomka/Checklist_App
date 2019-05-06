918b3445265634fa9bcf7026b2c65abebc812e88
John Slomka <jdslomka@icloud.com>
2019-05-06 10:53:09 -0400

    Initial Commit

---
+++ Checklist App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Checklist App
+//
+//  Created by John Slomka on 2019-05-06.
+//  Copyright © 2019 Life North Church. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
